df9603d4d81cc87d02063a9e6fbb217191e4ed89
GGpenin <ggpenin22@gmail.com>
2023-06-30 17:29:50 -0300

    entrega da semana/batalha naval

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ const seaBoard = [
 /*
   Desenvolva seu código logo abaixo
 */
+function allocateShips(shipPosicions, initialBoard) {
+  for (let i = 0 ; i < shipPosicions.length; i++) {
+    let position = shipPosicions[i];
+    initialBoard[position[0]][position[1]] = "S"
+  }
+return initialBoard;
+}
+
+function checkGuesses(guesses, mountedSeaBoard){
+ for (let i = 0; i < guesses.length; i++){
+  let assumptions = guesses[i];
+  if(mountedSeaBoard[assumptions[0]][assumptions[1]] === "S"){
+mountedSeaBoard[assumptions[0]][assumptions[1]] = "X";
+  }
+ }
+return mountedSeaBoard
+}
+
+function checkWinCondition(guessedSeaBoard){
+for ( let i = 0; i < guessedSeaBoard.length; i++) {
+  for (let j = 0; j < guessedSeaBoard[i].length; j++) {
+   if (guessedSeaBoard[i][j] == "S"){
+    return false;
+   }
+  }
+}
+return true;
+}
+
+
